b3041b34d210a410914353792af92e2161078dd3
omamesan <rx_7fd3sflame@yahoo.co.jp>
2019-06-05 11:11:37 +0000

    Finish login-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
   get  '/signup',  to: 'users#new'
+  root 'static_pages#top'
+  get '/signup', to: 'users#new'
+
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 end
